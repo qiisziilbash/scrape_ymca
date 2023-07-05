476f6fd2a9296c21b8fd4709632a6e6ed7e33d51
qiisziilbash <m.shafiei0423@gmail.com>
2023-07-05 16:18:00 -0500

    first draft

--- main.py
+++ main.py
@@ -1,26 +1,98 @@
-import datetime
 import os.path
+import logging
+import urllib.request
+from datetime import datetime
 
+import bs4
 from google.auth.transport import requests
 from google.oauth2 import credentials as oauth_credentials
 from google_auth_oauthlib import flow
 from googleapiclient import discovery
-from googleapiclient.errors import HttpError
 
+
+location = "carondeletpark"
+date = "2023-07-13"
+KEYWORD = "Swim"
 scopes = ["https://www.googleapis.com/auth/calendar"]
 calendar_id = "dc19c79d0c85db398ce294cb1a66256de1828e905e7bea388279aa1c8289f2cc@group.calendar.google.com"
+swimming_schedules_url = f"https://{location}reccomplex.virtuagym.com//classes/week/{date}?event_type=8&embedded=1&pref_club=37670"
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-def scrape_events(url):
+def scrape_events(url, date):
+    logger.info("Scraping the events from the given url ...")
+    url_reader = urllib.request.urlopen(url)
+    swimming_schedules_html = url_reader.read().decode("utf8")
+    url_reader.close()
+
+    soup = bs4.BeautifulSoup(swimming_schedules_html, features="html.parser")
+    reversed_date = "-".join(date.split("-")[::-1])
+    elements = soup.find_all(
+        "div", attrs={"class": f"internal-event-day-{reversed_date}"}
+    )
+
     events = []
+
+    for element in elements:
+        if KEYWORD in element.text:
+            events.append(element.text.replace("\n", "").strip())
+
+    logger.info(f"Scraped {len(events)} events at {date}!")
+
     return events
 
 
+def format_events(events):
+    formatted_events = []
+
+    for event in events:
+        event_parts = event.replace("     ", "$").split("$")
+        start_time = datetime.strptime(
+            event_parts[-1].split("-")[0].strip(), "%I:%M %p"
+        )
+        end_time = datetime.strptime(event_parts[-1].split("-")[1].strip(), "%I:%M %p")
+
+        formatted_events.append(
+            {
+                "description": event_parts[0],
+                "start_time": datetime.strftime(start_time, "%H:%M"),
+                "end_time": datetime.strftime(end_time, "%H:%M"),
+            }
+        )
+
+    logger.info("Formatted the events!")
+    return formatted_events
+
+
 def add_events(events):
-    pass
+    credentials = authenticate()
+    logger.info("Adding the events ...")
+    service = discovery.build("calendar", "v3", credentials=credentials)
+    for event in events:
+        add_event(event, service)
+
+
+def add_event(event, service):
+    body = {
+        "summary": event["description"],
+        "location": location,
+        "start": {
+            "dateTime": f"{date}T{event['start_time']}:00",
+            "timeZone": "America/Chicago",
+        },
+        "end": {
+            "dateTime": f"{date}T{event['end_time']}:00",
+            "timeZone": "America/Chicago",
+        },
+    }
+
+    event = service.events().insert(calendarId=calendar_id, body=body).execute()
 
 
 def authenticate():
+    logger.info("Authenticating ...")
     credentials = None
     if os.path.exists("token.json"):
         credentials = oauth_credentials.Credentials.from_authorized_user_file(
@@ -41,53 +113,11 @@ def authenticate():
     return credentials
 
 
-def test(credentials):
-    try:
-        service = discovery.build("calendar", "v3", credentials=credentials)
-
-        event = {
-            "summary": "test sum",
-            "location": "test loc",
-            "description": "test desc",
-            "start": {
-                "dateTime": "2023-07-05T09:00:00",
-                "timeZone": "America/Chicago",
-            },
-            "end": {
-                "dateTime": "2023-07-05T10:00:00",
-                "timeZone": "America/Chicago",
-            },
-        }
-
-        event = service.events().insert(calendarId=calendar_id, body=event).execute()
-
-        print("Getting the upcoming 10 events ...")
-        events = (
-            service.events()
-            .list(
-                calendarId=calendar_id,
-                timeMin=datetime.datetime.utcnow().isoformat() + "Z",
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-
-        for event in events.get("items", []):
-            start = event["start"].get("dateTime", event["start"].get("date"))
-            print(start, event["summary"])
-        else:
-            print("No upcoming events found.")
-            return
-
-    except HttpError as error:
-        print("An error occurred: %s" % error)
-
-
 def main():
-    credentials = authenticate()
-    test(credentials)
+    events = scrape_events(swimming_schedules_url, date)
+    events = format_events(events)
+    add_events(events)
+    logger.info("Events are succesefully added!")
 
 
 if __name__ == "__main__":
